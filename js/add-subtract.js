135435460cbba28c94b696feb3bb7cc89e686f18
Camila Garcia Federico <camilaabigailgf@gmail.com>
2023-10-11 19:02:58 -0300

    hago que el texto de edit item sea editable

--- js/add-subtract.js
+++ js/add-subtract.js
@@ -2,14 +2,30 @@ const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
 const quantity = document.getElementById("quantity");
 
+
 add.addEventListener("click", function () {
     quantity.value = parseInt(quantity.value) + 1;
 });
 
 subtract.addEventListener("click", function () {
-    if(quantity.value > 0) {
+    if (quantity.value > 0) {
         quantity.value = parseInt(quantity.value) - 1;
     }
 });
 
-console.log(add);
+/*
+const quantityInput = document.getElementById("quantity");
+
+function add() {
+    let value = parseInt(quantityInput.value);
+    value++;
+    quantityInput.value = value;
+}
+
+function subtract() {
+    let value = parseInt(quantityInput.value);
+    if (value > 0) {
+        value--;
+        quantityInput.value = value;
+    }
+}*/
