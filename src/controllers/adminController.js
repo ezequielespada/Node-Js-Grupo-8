56a88cdffbc01d46b944839aacf16800f170d720
Camila Garcia Federico <camilaabigailgf@gmail.com>
2023-11-30 18:38:36 -0300

    subo parte de admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,16 +1,26 @@
+const { json } = require('express');
 const productsData = require('../model/products.json');
 
 const adminControllers = {
     admin: (req, res)  => res.render('admin', {
-        products: productsData
+        products: productsData,
+        title: 'LISTADO DE PRODUCTOS',
+        add_item: 'AGREGAR',
+        id_title: 'ID',
+        codigo_title: 'Código',
+        nombre_title: 'Nombre del Producto',
+        categoria_title: 'Categoría'
+
         }
     ),
-    createget: (req, res)  => res.send('Vista admin create'),
+    createget: (req, res)  => res.render('create'),
     createpost: (req, res)  => res.send('Vista admin create post'),
-    editidget: (req, res)  => res.send(`Vista admin edit con id, siendo el id: ${req.params.id}`),
+    editidget: (req, res)  => res.render('edit', {
+        product: productsData.find(product => product.product_id == req.params.id),
+        
+    }),
     editidput: (req, res)  => res.send(`Vista admin edit con id put, siendo el id: ${req.params.id}`),
     deleteid: (req, res)  => res.send(`Vista admin delete con id, siendo el id: ${req.params.id}`)
-
 }
 
 module.exports = adminControllers;
