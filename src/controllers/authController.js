63c2deacec84b66f6b3322a7daff77d015bb70dd
Isabel <veranoflia@yahoo.com.ar>
2023-12-06 13:10:54 -0300

    AuthController(3 get) y shopcontroller(2 get)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const authControllers = {
-    loginget: (req, res)  => res.send('Vista login'),
+    loginget: (req, res)  => res.render('login'),
     loginpost: (req, res)  => res.send('Vista login post'),
-    registerget: (req, res)  => res.send('Vista register'),
+    registerget: (req, res)  => res.render('register'),
     registerpost: (req, res)  => res.send('Vista register post'),
     logout: (req, res)  => res.render('logout')
 }
--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,6 +1,12 @@
+const { json } = require('express');
+const productsData = require('../model/products.json');
+
 const shopControllers = {
-    shop: (req, res)  => res.send('Vista shop'),
-    itemid:  (req, res)  => res.send(`Vista shop item id, siendo el id: ${req.params.id}`),
+    shop: (req, res)  => res.render('shop'),
+    //itemid:  (req, res)  => res.send(`Vista shop item id, siendo el id: ${req.params.id}`),
+    itemid: (req, res)  => res.render('item', {
+        product: productsData.find(product => product.product_id == req.params.id),
+    }),
     itemidadd: (req, res)  => res.send('Vista añadir item al carrito'),
     cartget: (req, res)  => res.send('Vista del carrito'),
     cartpost: (req, res)  => res.send('Vista del carrito')
